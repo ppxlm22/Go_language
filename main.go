a9c3b9513a8958ffbf34950da9e92ca720d54806
Krittaphat  Ngampriam <krittaphatng@gmail.com>
2025-12-19 09:11:30 +0700

    feat: test use fiber get helloword

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
-import (
-	
-	"fmt"
-	"log"
-	"net/http"
-)
-func helloHandler (w http.ResponseWriter, r *http.Request) {
-if r.URL.Path != "/hello" {
-	http.Error(w , "404 not found", http.StatusNotFound)
-	return
-}
+import "github.com/gofiber/fiber/v2"
 
-	if r.Method != "GET"{
-		http.Error(w,"Method is not subported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "helloworld")
-}
 func main (){
-	http.HandleFunc("/hello", helloHandler)
+	app := fiber.New()
 
-	fmt.Printf("start server at port 8080\n")
-	if err := http.ListenAndServe(":8080",nil); err!= nil {
-		log.Fatal(err)
-	}
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("HelloWorld!")
+	})
+
+	app.Listen(":8080")
 }
