3e599a71628834e15f580c56fcb700d331126eb2
Krittaphat  Ngampriam <krittaphatng@gmail.com>
2025-12-19 09:02:30 +0700

    Test: use gofiber/v2

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
 import (
-	"github.com/ppxlm22/Go_language/palmNVS"
+	
+	"fmt"
+	"log"
+	"net/http"
 )
+func helloHandler (w http.ResponseWriter, r *http.Request) {
+if r.URL.Path != "/hello" {
+	http.Error(w , "404 not found", http.StatusNotFound)
+	return
+}
+
+	if r.Method != "GET"{
+		http.Error(w,"Method is not subported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "helloworld")
+}
 func main (){
-	palmNVS.SayHello()
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("start server at port 8080\n")
+	if err := http.ListenAndServe(":8080",nil); err!= nil {
+		log.Fatal(err)
+	}
 }
