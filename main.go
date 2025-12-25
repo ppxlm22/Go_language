54759d5c03cc577628c0b17f290eb9f92b5dfc63
Krittaphat  Ngampriam <krittaphatng@gmail.com>
2025-12-25 10:24:11 +0700

    feat: Separate the function from the main file

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"strconv"
 )
 
 type Employee struct{
@@ -28,71 +27,7 @@ func main (){
 
 	app.Listen(":8080")
 }
-func getEmployees(c *fiber.Ctx) error{
-	return c.JSON(employees)
-}
-func getEmployee(c *fiber.Ctx) error {
-	employeeID, err := strconv.Atoi(c.Params("IDem")) //แปลง String เป็นจำนวน Int
 
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error()) //ส่งกลับ error เป้นข้อความ
-	}
-	
-	for _, employee := range employees{
-		if employee.IDem == employeeID{
-			return c.JSON(employee)
-		}
-	}
-	return c.SendStatus(fiber.StatusNotFound) //ส่ง status404
-}
-func createEmployee (c *fiber.Ctx) error {
-	employee := new(Employee)
-
-	if err := c.BodyParser(employee) ; err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	employees = append(employees, *employee)
-	return c.JSON(employee )
-} 
-func updateEmployee (c *fiber.Ctx) error{
-	employeeID, err := strconv.Atoi(c.Params("IDem"))  //chack id update
-
-	if err != nil{
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	employeeUpdate := new(Employee)   //ตัวแทนรับอัพเดท
-
-	if err := c.BodyParser(employeeUpdate) ; err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, employee := range employees {
-		if employee.IDem == employeeID {
-			employees[i].Firstname = employeeUpdate.Firstname
-			employees[i].Lastname = employeeUpdate.Lastname
-			employees[i].Salary = employeeUpdate.Salary  //อนุญาตอัพเดท
-			
-			return c.JSON(employees[i])
-		}
-	}
-	return c.SendStatus(fiber.StatusNotFound)
-}
-
-func delectEmployee (c *fiber.Ctx) error {
-	employeeID, err := strconv.Atoi(c.Params("IDem"))
-
-	if err != err {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, employee := range employees {
-		if employee.IDem == employeeID{
-			employees = append(employees[:i],employees[i+1:]... )
-			return c.SendStatus(fiber.StatusNoContent)
-		}
-	}	
-	return c.SendStatus(fiber.StatusNoContent)
-}
 	
 
 
